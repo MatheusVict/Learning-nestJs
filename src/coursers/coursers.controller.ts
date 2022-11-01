99f899bc6b019a4cf52f320ed04a0452e20c9898
MatheusVict <matheusvictorhenrique@gmail.com>
2022-11-01 19:22:56 -0300

    Routes Dynamics

--- src/coursers/coursers.controller.ts
+++ src/coursers/coursers.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coursers')
 export class CoursersController {
   @Get('list')
   findAll(): string {
     return 'Todos';
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string): string {
+    return `Id: ${id}`;
   }
 }
